c98e2dfe58f073be3ca104449a27b1a3264c1b77
Thiago <studytmst@gmail.com>
2025-04-30 23:26:52 -0300

    chore: config the project using flyway

---
+++ src/main/java/com/cidadao_alerta/cidadao_alerta/CidadaoAlertaApplication.java
@@ -0,0 +1,13 @@
+package com.cidadao_alerta.cidadao_alerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadaoAlertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadaoAlertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidadao_alerta/cidadao_alerta/CidadaoAlertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadao_alerta.cidadao_alerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaoAlertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
