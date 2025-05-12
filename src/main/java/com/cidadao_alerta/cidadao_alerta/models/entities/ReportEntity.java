976215cd58945078334dddecc4f87422003c902d
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-05-12 12:18:59 -0300

    feat: add relation category report

--- src/main/java/com/cidadao_alerta/cidadao_alerta/models/entities/ReportEntity.java
+++ src/main/java/com/cidadao_alerta/cidadao_alerta/models/entities/ReportEntity.java
@@ -1,5 +1,7 @@
 package com.cidadao_alerta.cidadao_alerta.models.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +34,15 @@ public class ReportEntity {
 
   private String title;
   private String description;
+
+  @ManyToMany
+  @JoinTable(
+    name = "report_category",
+    joinColumns = @JoinColumn(name = "report_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+  )
+  @Builder.Default
+  private List<CategoryEntity> categories = new ArrayList<>();
 
   @Column(name = "image_url")
   private String imageUrl;
