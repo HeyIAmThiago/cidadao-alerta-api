25fb044f6024942ca4a3117fb17e9622dffef8a3
Thiago <studytmst@gmail.com>
2025-08-18 19:36:54 -0300

    chore: setting up junit with a trivial test

--- src/test/java/com/cidadao_alerta/cidadao_alerta/CidadaoAlertaApplicationTests.java
+++ src/test/java/com/cidadao_alerta/cidadao_alerta/CidadaoAlertaApplicationTests.java
@@ -1,13 +1,15 @@
 package com.cidadao_alerta.cidadao_alerta;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class CidadaoAlertaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("teste", "teste");
 	}
 
 }
