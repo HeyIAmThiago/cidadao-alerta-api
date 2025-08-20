11efbb4380090169c8bfc77fa67bce7b5c03081a
Thiago <studytmst@gmail.com>
2025-08-20 19:57:58 -0300

    fix:adjusting allure-report dependencies

--- src/test/java/com/cidadao_alerta/cidadao_alerta/TrivialTests.java
+++ src/test/java/com/cidadao_alerta/cidadao_alerta/TrivialTests.java
@@ -1,17 +1,18 @@
 package com.cidadao_alerta.cidadao_alerta;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class trivialTests {
+@Epic("Trivial Tests Suite")
+public class TrivialTests {
 
     @Test
+    @Feature("Trivial")
+    @Story("Check allure integration")
     void assertTrivialPass() {
         Assertions.assertEquals("4", "4");
-    }
-
-    @Test
-    void assertTrivialFail() {
-        Assertions.assertEquals("4", "8");
     }
 }
