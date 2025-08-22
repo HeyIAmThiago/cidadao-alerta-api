10f345e5ede46f229f8747611a63121e6acf9537
Thiago <studytmst@gmail.com>
2025-08-21 22:05:51 -0300

    feat: category docs

--- src/main/java/com/cidadao_alerta/cidadao_alerta/controllers/CategoryController.java
+++ src/main/java/com/cidadao_alerta/cidadao_alerta/controllers/CategoryController.java
@@ -3,6 +3,8 @@ package com.cidadao_alerta.cidadao_alerta.controllers;
 import com.cidadao_alerta.cidadao_alerta.models.entities.CategoryEntity;
 import com.cidadao_alerta.cidadao_alerta.repositories.CategoryRepository;
 import com.cidadao_alerta.cidadao_alerta.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,11 @@ import java.util.List;
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
-
+    @Operation(summary = "Find all categories",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Category found"),
+                    @ApiResponse(responseCode = "404", description = "No category found")
+            })
     @GetMapping
     List<CategoryEntity> findAll() {
         return categoryService.findAll();
