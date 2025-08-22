1ed003fcbdd39b098a4647b56797133a03bcd427
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-08-22 18:44:02 -0300

    fix: add missing ManyToOne import in ReportEntity

--- src/main/java/com/cidadao_alerta/cidadao_alerta/models/entities/ReportEntity.java
+++ src/main/java/com/cidadao_alerta/cidadao_alerta/models/entities/ReportEntity.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.FetchType;
